5e04137f1aae3b089d862d289763f3581616be17
aswanth6000 <aswanth682@gmail.com>
2024-01-02 09:34:58 +0530

    api gateway integration

---
+++ api-gateway/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy'
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+
+app.use('/auth', proxy("http://localhost:8000"))
+app.use('/user', proxy("http://localhost:8001"))
+app.use('/gig', proxy("http://localhost:8002"))
+app.use('/order', proxy("http://localhost:8003"))
+
+app.listen('8000', ()=>{
+    console.log("API Gateway Listening On Port 8000");
+})
