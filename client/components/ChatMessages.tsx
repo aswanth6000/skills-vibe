7eb26abf7c2ff7e53ce2e9f289f47541d8e78122
aswanth6000 <aswanth682@gmail.com>
2024-01-10 21:16:08 +0530

    chat messages

--- client/components/ChatMessages.tsx
+++ client/components/ChatMessages.tsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function ChatMessages() {
   return (
-    <div className='bg-red-300 w-1/2 h-screen'></div>
+    <div className='bg-bodywhite w-1/2 h-screen'>
+        <div className='w-full h-16 border-y flex items-center p-2 '>
+            <div  className="h-10 w-10 bg-black rounded-3xl"></div>
+            <div className='flex flex-col ml-2'>
+            <div className='text-md font-semibold'>Git tiltle goes here</div>
+            <div className='text-sm font-semibold'>Seller name goes here</div>
+            </div>
+        </div>
+            <div className='flex flex-col h-auto '>
+                {/* <div className='h-5/6 bg-black w-full'> df</div>
+                <div className=' bg-blue-600 w-full'>dxd</div> */}
+            </div>
+    </div>
   )
 }
--- client/components/chatAllUsers.tsx
+++ client/components/chatAllUsers.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function ChatAllUsers() {
   return (
-    <div className=" w-1/4 h-screen">
-      <div className="flex flex-row  mb-3 w-full justify-center mt-3">
+    <div className=" w-1/4 h-screen overflow-scroll">
+      <div className="flex flex-row   mb-3 w-full justify-center mt-3">
         <input
           type="text"
-          className="pl-1 p-2 shadow-md outline-none rounded-s-md border bg-bodywhite"
+          className="pl-1 p-2 shadow-md outline-none rounded-s-md border bg-bodywhite h-10"
           placeholder="search"
         />
-        <button className="bg-green-600 p-1 rounded-e-md shadow-md border">
+        <button className="bg-green-600 p-1 rounded-e-md shadow-md border h-10">
           Search
         </button>
       </div>
-      <div className="flex flex-col h-screen  w-full items-center  overflow-x-scroll">
+      <div className="flex flex-col h-fit  w-full items-center">
         {/* users */}
         <div className="bg-bodywhite  h-16 rounded-md shadow-md w-11/12 flex border  mb-2 items-center p-2">
           <div className="h-10 w-10 bg-black rounded-3xl"></div>
