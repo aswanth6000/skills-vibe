5b6cd3a906789a4039dc5635c898d97c9ac79fb8
aswanth6000 <aswanth682@gmail.com>
2024-01-10 21:35:39 +0530

    done

--- client/components/ChatSenderInfo.tsx
+++ client/components/ChatSenderInfo.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function ChatSenderInfo() {
   return (
-    <div className='bg-blue-300 w-1/4 h-screen'></div>
+    <div className='  flex flex-col items-center  w-1/4 h-screen'>
+        <div  className="h-48 w-48 bg-black rounded-full mt-16"></div>
+        <div className=''><button className='bg-blue-400 p-1 mt-4 font-semibold text-white rounded-md'>View Profile</button></div>
+        <div className='flex flex-col mt-3 items-center'>
+            <h1>Gig title</h1>
+            <p>Gig description</p>
+        </div>
+    </div>
   )
 }
