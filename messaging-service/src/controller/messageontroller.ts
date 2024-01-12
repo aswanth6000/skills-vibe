6d4e90a0385acf077b37eda551ed2c56a94e7c4b
aswanth6000 <aswanth682@gmail.com>
2024-01-12 20:55:43 +0530

    msg controller

--- messaging-service/src/controller/messageontroller.ts
+++ messaging-service/src/controller/messageontroller.ts
@@ -1,4 +1,6 @@
 import { UserModel, User } from "../model/userModel"
+import { chatModel } from "../model/chatModel"
+import { Request, Response } from "express"
 import userMessageConsumers from "../events/userMessageConsumer"
 
 
@@ -21,6 +23,23 @@ const messageController = {
     async userUpdateSave(){
         const userData: any = await userMessageConsumers.userUpdatedMessageConsumer()
         await UserModel.findByIdAndUpdate({userId: userData._id}, {userData}, {new: true})
+    },
+    async accessChat(req:Request, res: Response){
+        const {userId} = req.body;
+        if (!userId) {
+            console.log("UserId param not sent with request");
+            return res.status(400).json({ message: 'nouserFound' });
+        }
+        
+          var isChat = await chatModel.find({
+            isGroupChat: false,
+            $and: [
+              { users: { $elemMatch: { $eq: req.user._id } } },
+              { users: { $elemMatch: { $eq: userId } } },
+            ],
+          })
+            .populate("users")
+            .populate("latestMessage");
     }
 }
 
