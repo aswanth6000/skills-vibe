8a46bd92ae2366f42a1bb063fa7f61da56d9c097
aswanth6000 <aswanth682@gmail.com>
2024-01-13 15:05:57 +0530

    back

--- messaging-service/src/controller/messagecontroller.ts
+++ messaging-service/src/controller/messagecontroller.ts
@@ -28,62 +28,69 @@ const messageController = {
     await UserModel.findByIdAndUpdate(userData._id, { userData }, { new: true })
   },
   async accessChat(req: Request, res: Response) {
-    const { userId } = req.body;
-    if (!userId) {
-      console.log("UserId param not sent with request");
-      return res.status(400).json({ message: 'nouserFound' });
-    }
+  //   const { userId } = req.body;
+  //   if (!userId) {
+  //     console.log("UserId param not sent with request");
+  //     return res.status(400).json({ message: 'nouserFound' });
+  //   }
 
-    const token = req.headers.authorization?.split(' ')[1]
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized - Token not provided' });
-    }
+  //   const token = req.headers.authorization?.split(' ')[1]
+  //   if (!token) {
+  //     return res.status(401).json({ error: 'Unauthorized - Token not provided' });
+  //   }
 
-    let decodedToken: JwtPayload;
+  //   let decodedToken: JwtPayload;
 
-    try {
-      decodedToken = jwt.verify(token, jwtSecret) as JwtPayload;
-    } catch (jwtError) {
-      console.log('JWT Verification Error:', jwtError);
-      return res.status(401).json({ error: 'Unauthorized - Invalid token' });
-    }
+  //   try {
+  //     decodedToken = jwt.verify(token, jwtSecret) as JwtPayload;
+  //   } catch (jwtError) {
+  //     console.log('JWT Verification Error:', jwtError);
+  //     return res.status(401).json({ error: 'Unauthorized - Invalid token' });
+  //   }
 
-    console.log('Decoded Token:', decodedToken);
-    const myId = decodedToken.userId
-    var isChat: any = await chatModel.find({
-      $and: [
-        { users: { $elemMatch: { $eq: myId } } },
-        { users: { $elemMatch: { $eq: userId } } },
-      ],
-    })
-      .populate("users")
-      .populate("latestMessage");
+  //   console.log('Decoded Token:', decodedToken);
+  //   const myId = decodedToken.userId
+  //   var isChat: any = await chatModel.find({
+  //     $and: [
+  //       { users: { $elemMatch: { $eq: myId } } },
+  //       { users: { $elemMatch: { $eq: userId } } },
+  //     ],
+  //   })
+  //     .populate("users")
+  //     .populate("latestMessage");
 
-    isChat = await UserModel.populate(isChat, {
-      path: "latestMessage.sender",
-      select: "username, profilePicture, email",
-    });
+  //   isChat = await UserModel.populate(isChat, {
+  //     path: "latestMessage.sender",
+  //     select: "username, profilePicture, email",
+  //   });
 
-    if (isChat.length > 0) {
-      res.send(isChat[0]);
-    } else {
-      var chatData = {
-        chatName: "sender",
-        isGroupChat: false,
-        users: [myId, userId],
-      };
+  //   if (isChat.length > 0) {
+  //     res.send(isChat[0]);
+  //   } else {
+  //     var chatData = {
+  //       chatName: "sender",
+  //       isGroupChat: false,
+  //       users: [myId, userId],
+  //     };
 
-      try {
-        const createdChat = await chatModel.create(chatData);
-        const FullChat = await chatModel.findOne({ _id: createdChat._id }).populate(
-          "users",
-        );
-        res.status(200).json(FullChat);
-      } catch (error) {
-        res.status(400).json({message: 'page not found'})
-      }
-    }
+  //     try {
+  //       const createdChat = await chatModel.create(chatData);
+  //       const FullChat = await chatModel.findOne({ _id: createdChat._id }).populate(
+  //         "users",
+  //       );
+  //       res.status(200).json(FullChat);
+  //     } catch (error) {
+  //       res.status(400).json({message: 'page not found'})
+  //     }
+  //   }
+  // },
+  console.log('sdfsedf');
+  
   },
+  test(req: Request, res: Response){
+    console.log("hello");
+    
+  }
 }
 
 export default messageController
--- messaging-service/src/routes/messageRouter.ts
+++ messaging-service/src/routes/messageRouter.ts
@@ -3,6 +3,7 @@ import messageController from '../controller/messagecontroller';
 const router = express.Router();
 
 router.post('/accesschat', messageController.accessChat);
+router.get('/test', messageController.test)
 
 
 export default router
