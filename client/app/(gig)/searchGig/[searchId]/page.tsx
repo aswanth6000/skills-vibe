a499b97808391e94a9b918d2386dbad792f99c8a
aswanth6000 <aswanth682@gmail.com>
2024-01-24 19:28:51 +0530

    search complete

--- client/app/(gig)/searchGig/[searchId]/page.tsx
+++ client/app/(gig)/searchGig/[searchId]/page.tsx
@@ -8,6 +8,7 @@ import { Autoplay } from "swiper/modules";
 import Image from "next/image";
 import { Gigs } from "@/types/gigTypes";
 import Link from "next/link";
+import Grid from "@/components/grid";
 
 
 let bearerToken: string | null;
@@ -45,83 +46,15 @@ const Page: React.FC<GridProps> = () => {
 
   return (
     <>
-      <Navbar />
-
-      <div className='flex flex-row flex-wrap mb-5'>
-      {data.map((x) => (
-        <div key={x.refId} className='w-56 h-auto flex flex-col justify-start border rounded-2xl mt-3 mb-3 ml-3'>
-          <div>
-            {/* Initialize Swiper component for image carousel */}
-            <Swiper
-              spaceBetween={50}
-              slidesPerView={1}
-              modules={[Autoplay]}
-              autoplay={{ delay: 5000 }}
-              loop={true}
-              className='h-auto w-full'
-            >
-              <SwiperSlide>
-                <div>
-                  <Image
-                  height={500}
-                  width={500}
-                    src={x.image1}
-                    className='w-full h-36 object-fill m-0 p-0 rounded-2xl'
-                    alt='Slide 1'
-                  />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div>
-                  <Image
-                  height={500}
-                  width={500}
-                    src={x.image2}
-                    className='w-full h-36 object-fill m-0 p-0 rounded-2xl'
-                    alt='Slide 2'
-                  />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div>
-                  <Image
-                  height={500}
-                  width={500}
-                    src={x.image3}
-                    className='w-full h-36 object-fill m-0 p-0 rounded-sm'
-                    alt='Slide 3'
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-          <div>
-            <p className='pl-3 font-bold'>{x.title}</p>
-          </div>
-          <div>
-            {/* You can replace this placeholder with your actual star rating component */}
-            <p className='pl-3'>Star rating here</p>
-          </div>
-          <div>
-            <p className='pl-3 font-semibold'>From: {x.price}</p>
-          </div>
-          {/* <div className='pl-3'>
-            <a href={`/editgig/${x.refId}`} className='text-blue-900'>
-              Edit
-            </a>
-          </div> */}
-          <div className='pl-3'>
-          <Link href={`/gigview/${x.refId}`}>
-          <button className='text-white bg-green-600 hover:bg-green-900 rounded-md w-11/12 mb-4'>
-              View Details
-            </button>
-            </Link>
-          </div>
-          
-
-        </div>
-      ))}
-    </div>
+      {/* <Navbar /> */}
+      
+      {data.length > 0 && <h1 className="text-2xl font-bold">Search results for "{searchId}"</h1>}
+      {data.length > 0 ? 
+      <Grid props={data}/> : 
+      <div className="w-full flex justify-center h-screen items-center">
+        <h1 className="text-2xl font-bold">No items found for the search "{searchId}"</h1>
+      </div>
+      }
     </>
   );
 }
