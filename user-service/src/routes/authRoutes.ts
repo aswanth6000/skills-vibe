77f4a972ce1ace8046572628f193452b5a2c1ff3
aswanth6000 <aswanth682@gmail.com>
2024-01-31 17:34:50 +0530

    api gateway cinfigured

--- user-service/src/routes/authRoutes.ts
+++ user-service/src/routes/authRoutes.ts
@@ -3,10 +3,10 @@ import authController from "../controller/authController";
 
 const router = express.Router();
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-router.post('/sendotp', authController.sendOtp);
-router.post('/submitotp', authController.submitOtp);
-router.post('/submitpassword', authController.changePassword)
+router.post("/user/signup", authController.signup);
+router.post("/user/login", authController.login);
+router.post('/user/sendotp', authController.sendOtp);
+router.post('/user/submitotp', authController.submitOtp);
+router.post('/user/submitpassword', authController.changePassword)
 
 export default router;
