868e3e72e5e729a2de831e0046243db69ad662eb
aswanth6000 <aswanth682@gmail.com>
2024-02-06 19:34:11 +0530

    depl

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,25 +1,11 @@
 'use client'
-import Navbar from "@/components/navbar"
-import { CarouselDefault } from "@/components/carousel"
-import Grid from '@/components/grid'
-import Popular from '@/components/popular'
-import Footer from "@/components/footer"
-export default function Home() {
-  const styles = {
-    outerdiv : 'flex h-full w-full items-center justify-center',
-    innerdiv : 'w-full h-96 object-fill m-0 p-0',
-    autosec : 6000
-  }
+import Home from "./(user)/userhome/page"
+
+export const page = () => {
   return (
-    <div className="bg-bodywhite">
-    <Navbar/>
-    <CarouselDefault stl={styles}/>
-    <Grid/>
-    <Grid/>
-    <Grid/>
-    <Grid/>
-    <Popular/>
-    <Footer/>
-    </div>
+    <>
+      <Home/>
+    </>
   )
 }
+
