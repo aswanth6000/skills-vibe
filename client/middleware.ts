2d0098512ac22f6a96814608ac81f50524a8a997
aswanth6000 <aswanth682@gmail.com>
2024-02-06 20:57:48 +0530

    middleware deleted

--- client/middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-export function authenticationMiddleware(request: NextRequest) {
-    if (!localStorage.getItem("token")) {
-      // Redirect to the login page
-      return NextResponse.redirect('/login');
-    }
-    return NextResponse.next();
-  }
