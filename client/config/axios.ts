d480a6d0427170a9b27ea7905c911b5f4b94fc40
aswanth6000 <aswanth682@gmail.com>
2024-02-09 01:27:04 +0530

    base

--- client/config/axios.ts
+++ client/config/axios.ts
@@ -2,17 +2,17 @@ import axios from "axios";
 
 const baseUrl: string = 'https://gadgetease.online';
 
-axios.interceptors.request.use(
-    (config)=>{
-    const token = localStorage.getItem("token");
-    if(token){        
-        config.headers['Authorization'] = `Bearer ${token}`;
-    };
-    return config
-},(error)=>{
-    Promise.reject(error)
-}
-)
+// axios.interceptors.request.use(
+//     (config)=>{
+//     const token = localStorage.getItem("token");
+//     if(token){        
+//         config.headers['Authorization'] = `Bearer ${token}`;
+//     };
+//     return config
+// },(error)=>{
+//     Promise.reject(error)
+// }
+// )
 
 const instance = axios.create({
     baseURL : baseUrl
