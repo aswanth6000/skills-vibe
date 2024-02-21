8dc52c407e4d215a7efcf03264ad869e4b6656b3
aswanth6000 <aswanth682@gmail.com>
2024-02-21 14:38:31 +0530

    chat

--- client/app/(communication)/messages/page.tsx
+++ client/app/(communication)/messages/page.tsx
@@ -31,7 +31,7 @@ import io from "socket.io-client";
 import Navbar from "@/components/navbar";
 import { useRouter } from "next/navigation";
 
-const ENDPOINT = "https://gadgetease.online/socket.io";
+const ENDPOINT = "https://gadgetease.online";
 var socket: any, selectedChatCompare: any;
 
 interface Pokedex {
